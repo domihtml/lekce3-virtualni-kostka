d9226cafd4dac535d53f7a1e3555f70d1b85f274
domihtml <dvorak.dominika@gmail.com>
2021-04-04 10:28:22 +0200

    kostka

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // tady je místo pro náš program
+
+let zprava = document.getElementById("zprava");
+let kostka = document.getElementById("kostka");
+
+
+let cislo = Math.floor(Math.random() * 6) + 1;
+
+function GenerujCislo (){
+let cislo = Math.floor(Math.random() * 6) + 1;
+    
+    console.log(cislo);
+ 
+    if (cislo ===6) {
+    zprava.innerHTML = "Hodil/a jsi " + cislo + ". Výhra!";
+    }
+    else 
+    zprava.innerHTML = "Smůla. Hodil/a jsi " + cislo + ". Házej znovu";
+
+    kostka.src = "obrazky/" + cislo + ".png";
+    }
+
+
